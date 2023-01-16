8ab6a619672ef4763ca60cf0d9a8a48279a4253a
Felipe Palacio <felipepalaciobetancourt@gmail.com>
2023-01-16 16:38:52 +0100

    Start

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
 import './App.css';
+import useGenerateRandomColor from './useGenerateRandomColor';
 
 function App() {
-  return <div></div>;
+  const { color, generateColor } = useGenerateRandomColor();
+  return (
+    <div
+      style={{
+        height: '100vh',
+        width: '100vw',
+        backgroundColor: '#' + color,
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <button
+        style={{
+          padding: '20px',
+          borderRadius: '40px',
+          backgroundImage: '(#a8edea 0%, #fed6e3 100%)',
+          fontSize: 'smaller',
+        }}
+        onClick={generateColor}
+      >
+        Generate
+      </button>
+    </div>
+  );
 }
 
 export default App;
